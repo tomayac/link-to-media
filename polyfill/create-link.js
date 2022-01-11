ffd565bdccd09770387063dd188f57a0ac0b5177
Thomas Steiner <tomac@google.com>
2022-01-11 13:15:05 +0100

    Fix id selector

--- polyfill/create-link.js
+++ polyfill/create-link.js
@@ -2,7 +2,7 @@ const BOILERPLATE = '#:~:selector(type=CssSelector,value=';
 
 function createLink(element) {
   if (element.id) {
-    return `${location.href}${BOILERPLATE}${element.id})`;
+    return `${location.href}${BOILERPLATE}#${element.id})`;
   }
   if (element.src) {
     return `${location.href}${BOILERPLATE}img[src="${element.src}"])`;
