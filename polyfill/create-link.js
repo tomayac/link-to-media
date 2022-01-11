54c07807ed6856fdd855e3119e84ca944f54e88f
Thomas Steiner <tomac@google.com>
2022-01-11 18:26:25 +0100

    Start :has selector stuff

--- polyfill/create-link.js
+++ polyfill/create-link.js
@@ -1,16 +1,34 @@
 function createSelector(element) {
+  // `<foo id>`
   if (element.id) {
     return `#${CSS.escape(element.id)}`;
   }
+  const tag = element.tagName.toLowerCase();
+  // `<foo src>`
   if (element.src) {
-    const tag = element.tagName.toLowerCase();
     return `${tag}[src="${element.src.replace(/["\\]/g, '\\$&')}"]`;
+  }
+  const childWithSrcAttribute = element.querySelector('[src]');
+  // `<foo><bar src /></foo>`
+  if (childWithSrcAttribute) {
+    // @ToDo: Need to fix this and understand how `:has` works
+    // (https://www.smashingmagazine.com/2021/06/has-native-css-parent-selector/).
+    return `${tag}:has([src=${childWithSrcAttribute.src.replace(/["\\]/g, '\\$&')}])`;
   }
   throw new Error('unsupported element');
 }
 
+function isUnique(selector) {
+  return document.querySelectorAll(selector).length === 1;
+}
+
 function createLink(element) {
   const selector = createSelector(element);
+  if (isUnique(selector)) {
+    // @ToDo: Only proceed if the selector is unique. Otherwise, find a new
+    // selector.
+    console.log('Selector is unique', selector);
+  }
   return new URL(
     `#:~:selector(type=CssSelector,value=${selector})`,
     location.href,
