7323432744a95ad52ce6d69a77b45f1f976db071
Ingvar Stepanyan <rreverser@google.com>
2022-01-12 15:11:22 +0000

    Flatten scrollTo function with async-await

--- polyfill/scroll-to.js
+++ polyfill/scroll-to.js
@@ -14,8 +14,18 @@
  * limitations under the License.
  */
 
+function waitForWindowEvent(event) {
+  return new Promise((resolve) =>
+    addEventListener(event, resolve, { once: true }),
+  );
+}
+
+function waitForAnimationFrame() {
+  return new Promise((resolve) => requestAnimationFrame(resolve));
+}
+
 // Via https://github.com/flackr/scroll-to-extension/blob/main/src/scroll-to.js.
-function scrollTo() {
+async function scrollTo() {
   const regExp = /[^#]*#.*:~:selector\(type=CssSelector,value=(.*)\)$/;
   // Scroll to image only supports a simple or compound selector selecting on
   // type, id, class and attribute selectors with a specific list of allowed
@@ -44,22 +54,15 @@ function scrollTo() {
   elem.style.outline = 'Highlight solid 3px';
   // Need to wait for the load event to ensure the scroll does not get
   // undone by the browsers anchor link scroll behavior.
-  window.addEventListener('load', () => {
-    // Wait until the second frame after the load to scroll the element into
-    // view. Scrolling immediately seems to sometimes fail.
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        elem.scrollIntoView();
-        // Wait until the element is scrolled into view to add a listener to
-        // cancel the highlight.
-        requestAnimationFrame(() => {
-          window.addEventListener(
-            'scroll',
-            () => (elem.style.outline = previousOutline),
-            { once: true },
-          );
-        });
-      });
-    });
-  });
+  await waitForWindowEvent('load');
+  // Wait until the second frame after the load to scroll the element into
+  // view. Scrolling immediately seems to sometimes fail.
+  await waitForAnimationFrame();
+  await waitForAnimationFrame();
+  elem.scrollIntoView();
+  await waitForAnimationFrame();
+  // Wait until the element is scrolled into view to add a listener to
+  // cancel the highlight.
+  await waitForWindowEvent('scroll');
+  elem.style.outline = previousOutline;
 }
